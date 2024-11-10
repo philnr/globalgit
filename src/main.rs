3e6d4d17beee6fb638928e030eda8b4712b28cf8
philnr <nestler@e-clearing.net>
2024-11-10 17:02:51 +0100

    added filtering by author name

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use colored::*;
 use std::io::Write;
 
 fn main() {
-    // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 3 {
@@ -18,15 +17,19 @@ fn main() {
 
     let mode = &args[1];
     let root_dir = &args[2];
+    let mut user = "";
+
+    if args.len() == 4 {
+        user = &args[3];
+    }
 
     match mode.as_str() {
         "repos" => list_repos(root_dir),
-        "log" => print_combined_log(root_dir),
+        "log" => print_combined_log(root_dir, user),
         _ => eprintln!("Invalid mode. Use 'repos' or 'log'."),
     }
 }
 
-/// Lists all git repositories in the given root directory.
 fn list_repos(root_dir: &str) {
     let mut walker = WalkDir::new(root_dir).into_iter();
     while let Some(entry) = walker.next() {
@@ -36,17 +39,16 @@ fn list_repos(root_dir: &str) {
                     let path = entry.path();
                     if is_git_repo(path) {
                         println!("Found git repo {}", get_repo_name(path));
-                        walker.skip_current_dir(); // Skip the current directory
+                        walker.skip_current_dir();
                     }
                 }
             }
-            Err(_) => continue, // Ignore errors like permission issues
+            Err(_) => continue,
         }
     }
 }
 
-/// Prints the combined git log of all git repositories in the given root directory.
-fn print_combined_log(root_dir: &str) {
+fn print_combined_log(root_dir: &str , user: &str) {
     let mut walker = WalkDir::new(root_dir).into_iter();
     let mut log_entries = Vec::new();
 
@@ -61,59 +63,59 @@ fn print_combined_log(root_dir: &str) {
                             .arg("-C")
                             .arg(path)
                             .arg("log")
-                            .arg("--pretty=format:%h %ct %s")
+                            .arg("--pretty=format:%h %ct %an %s")
                             .output()
                             .expect("Failed to execute git command");
 
                         let logs = String::from_utf8_lossy(&output.stdout);
                         for line in logs.lines() {
-                            let parts: Vec<&str> = line.splitn(3, ' ').collect();
-                            if parts.len() == 3 {
+                            let parts: Vec<&str> = line.splitn(4, ' ').collect();
+                            if user != "" && parts[2] != user {
+                                continue;
+                            }
+                            if parts.len() == 4 {
                                 let timestamp: i64 = parts[1].parse().unwrap_or(0);
-                                log_entries.push((timestamp, parts[0].to_string(), parts[2].to_string(), repo_name.clone()));
+                                log_entries.push((timestamp, parts[0].to_string(), parts[2].to_string(), parts[3].to_string(), repo_name.clone()));
                             }
                         }
-                        walker.skip_current_dir(); // Skip the current directory since git repo has already been found
+                        walker.skip_current_dir();
                     }
                 }
             }
-            Err(_) => continue, // Ignore errors like permission issues
+            Err(_) => continue,
         }
     }
 
-    // Sort log entries by timestamp in descending order and take the latest 10
     log_entries.sort_by_key(|entry| Reverse(entry.0));
-    // Create a command to pipe the output to `less`
     let mut less = Command::new("less")
         .stdin(Stdio::piped())
         .spawn()
         .expect("Failed to start less");
     {
         let stdin = less.stdin.as_mut().expect("Failed to open stdin");
-        for (timestamp, hash, message, repo_name) in log_entries {
+        for (timestamp, hash, author, message, repo_name) in log_entries {
             let datetime = Utc.timestamp_opt(timestamp, 0).single().expect("Invalid timestamp");
             let formatted_date = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
             writeln!(
                 stdin,
-                "{} {} {} \n{}",
+                "{} {} {} \n{} {}",
                 formatted_date.truecolor(114, 135, 253), 
-                hash.truecolor(92, 95, 119),           
-                repo_name.truecolor(254, 100, 11),     
-                message.truecolor(255, 255, 224)       
+                repo_name.truecolor(254, 100, 11),
+                hash.truecolor(92, 95, 119),    
+                author.truecolor(10, 10, 10),           
+                message.truecolor(255, 255, 224),
             ).expect("Failed to write to less");    
         }
     }
-    
+    less.wait().expect("Failed to wait on less");
 }
 
-/// Checks if the given path is a Git repository.
 fn is_git_repo(path: &Path) -> bool {
     path.join(".git").exists()
 }
 
-/// Retrieves the name of the Git repository (i.e., the name of the directory).
 fn get_repo_name(path: &Path) -> String {
     path.file_name()
-        .and_then(|name| name.to_str()) // Get the name of the directory
-        .unwrap_or("Unknown").to_string() // If it's not valid, return "Unknown"
+        .and_then(|name| name.to_str())
+        .unwrap_or("Unknown").to_string()
 }
